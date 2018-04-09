c42921f474a5265da75b091944f95597dfac40c5
JoshCplusplus <comes_0807806@erudite.members.linode.com>
2018-04-09 13:27:52 -0700

    Fixed a bug with a constructor in the atom.h

--- atom.h
+++ atom.h
@@ -6,6 +6,14 @@ using namespace std;
 
 class atom{
 	string name;
+	public:
+	atom(string atomname){
+		name = atomname;
+	}
+	atom();
+	string get_name(){
+		return name;
+	} 
 };
 
 class molecule{
