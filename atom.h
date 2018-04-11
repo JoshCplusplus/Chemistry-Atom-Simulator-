b92f04c0e1df498bb7ebc8b0c178150694929abf
JoshCplusplus <comes_0807806@erudite.members.linode.com>
2018-04-10 17:13:30 -0700

    Changed the atom 1 and atom 2 strings into a pair

--- atom.h
+++ atom.h
@@ -5,33 +5,28 @@ using namespace std;
 
 class molecule{
 	string name;
-	string first;
-	string second;
+	pair <string,string> madeof;
 	double energy;
 	string message;
 	public:
-	double get_energy(){
+	double get_energy() const{
 		return energy;
 	}
 	string get_name(){
 		return name;
 	}
-	string get_first(){
-		return first;
-	}
-	string get_second(){
-		return second;
-	}
 	string get_message(){
 		cout << message << endl;
 	}
-
+	pair<string,string> get_pair(){
+		return madeof;
+	}
 	molecule(string name1, string atom1, string atom2, double energy1,string newmessage){
 		name = name1;
-		first = atom1;
-		second = atom2;
+		madeof = make_pair(atom1,atom2);
 		energy = energy1;
 		message = newmessage;
 	}
 }h2("H2","H","H",20,"Hydrogen Bond Formed"),h2o("H2O","H2","O",286,"Water Created"); 
+
 
