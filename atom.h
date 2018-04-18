8f9232ab89db542cf4783b0642bea9641c4ce6de
Desiyah <38126532+Desiyah@users.noreply.github.com>
2018-04-18 13:20:56 -0700

    Update atom.h

--- atom.h
+++ atom.h
@@ -31,10 +31,13 @@ class molecule{
 		return name;
 	}
 	string get_message(){
-		cout << message << endl;
+		return message;
 	}
 	pair<string,string> get_pair(){
 		return madeof;
+	}
+	molecule(){
+		name = "BAD";
 	}
 	molecule(string name1, string atom1, string atom2, double energy1,string newmessage){
 		name = name1;
@@ -42,6 +45,6 @@ class molecule{
 		energy = energy1;
 		message = newmessage;
 	}
-}h2("H2","H","H",20,"Hydrogen Bond Formed"),h2o("H2O","H2","O",286,"Water Created"); 
+}h2("H2","H","H",432,"Hydrogen Bond Formed"),h2o("H2O","H2","O",934,"Water Created"),o2("O2","O","O",204,"Oxygen Bond Formed"),co2("CO2","C","O2",716,"Carbon Dyoxide Formed"),no("NO","N","O",201,"Nitrogen Oxide Formed"),cl2("CL2","CL","Cl",242,"Chlorine Gas Formed"),n2("N2","N","N",163,"Nitrogen Gas Formed"),no2("NO2","N","O2",402,"Nitrogen Oxide Formed"),hf("HF","H","F",567,"Hydrogen Fluoride Formed");
 
 
